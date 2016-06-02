76e46b02e7418be825cb9efeea91d9d790248de3
Fr-d-rik <frederikharder@gmail.com>
2016-06-02 16:24:45 +0200

    mnist examples

---
+++ mnist_autoencoder.py
@@ -0,0 +1,75 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+from keras import regularizers
+import numpy as np
+import matplotlib.pyplot as plt
+
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.activity_l1(10e-5))(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input=input_img, output=decoded)
+
+# this model maps an input to its encoded representation
+encoder = Model(input=input_img, output=encoded)
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(input=encoded_input, output=decoder_layer(encoded_input))
+
+
+# First, we'll configure our model to use a per-pixel binary crossentropy loss, and the Adadelta optimizer:
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print x_train.shape
+print x_test.shape
+
+
+autoencoder.fit(x_train, x_train,
+                nb_epoch=10,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(1,n+1):
+    # display original
+    ax = plt.subplot(2, n, i)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
