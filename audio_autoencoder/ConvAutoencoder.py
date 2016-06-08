1903fcc118ec66ea51b0ed4c65ee828c88ebe948
toologicbv <jorg.sander@toologic.com>
2016-06-08 12:21:42 +0200

    Last version conv autoencoder with pretty good reconstruction results

--- audio_autoencoder/ConvAutoencoder.py
+++ audio_autoencoder/ConvAutoencoder.py
@@ -1,5 +1,6 @@
 from keras.layers import Input, Convolution1D, MaxPooling1D, UpSampling1D, ZeroPadding1D
 from keras.models import Model
+from keras.layers import BatchNormalization
 import matplotlib.pyplot as plt
 import numpy as np
 from keras import backend as K
@@ -21,23 +22,29 @@ class ConvAutoencoder:
 
         assert fdim % 8 == 0, "max pools require input divisible by 2^3"
 
-        x = Convolution1D(16, 3, activation='relu', border_mode='same')(input_img)
+        x = Convolution1D(32, 35, activation='relu', border_mode='same')(input_img)
+        x = BatchNormalization()(x)
         x = MaxPooling1D(2, border_mode='valid')(x)
-        x = Convolution1D(8, 3, activation='relu', border_mode='same')(x)
+        x = Convolution1D(16, 7, activation='relu', border_mode='same')(x)
+        x = BatchNormalization()(x)
         x = MaxPooling1D(2, border_mode='valid')(x)
-        x = Convolution1D(8, 3, activation='relu', border_mode='same')(x)
+        x = Convolution1D(4, 3, activation='relu', border_mode='same')(x)
+        x = BatchNormalization()(x)
         encoded = MaxPooling1D(2, border_mode='valid')(x)
 
-        x = Convolution1D(8, 3, activation='relu', border_mode='same')(encoded)
+        x = Convolution1D(4, 3, activation='relu', border_mode='same')(encoded)
+        x = BatchNormalization()(x)
         x = UpSampling1D(2)(x)
-        x = Convolution1D(8, 3, activation='relu', border_mode='same')(x)
+        x = Convolution1D(16, 7, activation='relu', border_mode='same')(x)
+        x = BatchNormalization()(x)
         x = UpSampling1D(2)(x)
-        x = Convolution1D(16, 3, activation='relu', border_mode='same')(x)
+        x = Convolution1D(32, 35, activation='relu', border_mode='same')(x)
+        x = BatchNormalization()(x)
         x = UpSampling1D(2)(x)
         decoded = Convolution1D(1, 3, activation='tanh', border_mode='same')(x)
 
         self.autoencoder = Model(input=input_img, output=decoded)
-        self.autoencoder.compile(optimizer='adagrad', loss='binary_crossentropy')
+        self.autoencoder.compile(optimizer='rmsprop', loss='mse')  # binary_crossentropy
 
         self.autoencoder.summary()
 
@@ -54,19 +61,20 @@ class ConvAutoencoder:
         decoded_imgs = self.autoencoder.predict(self.test_data)
 
         num_datapoints = 20
+        offset = 100
         n = 2
         plt.figure(figsize=(20, 4))
         for i in range(1, n+1):
             # display original
             ax = plt.subplot(2, n, i)
-            plt.plot(self.test_data[i][:num_datapoints])
+            plt.plot(self.test_data[i][offset:offset + num_datapoints])
             plt.gray()
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
 
             # display reconstruction
             ax = plt.subplot(2, n, i + n)
-            plt.plot(decoded_imgs[i][:num_datapoints])
+            plt.plot(decoded_imgs[i][offset:offset + num_datapoints])
             plt.gray()
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
