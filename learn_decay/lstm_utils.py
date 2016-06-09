696982ce1d8427878962e5d4a638e2dde7d9cbf8
Fr-d-rik <frederikharder@gmail.com>
2016-06-09 14:19:43 +0200

    updated lstm generator

--- learn_decay/lstm_utils.py
+++ learn_decay/lstm_utils.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
-from keras.layers.core import TimeDistributedDense
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.core import Dense
 from keras.layers.recurrent import LSTM
 
 
@@ -8,10 +9,10 @@ from keras.layers.recurrent import LSTM
 def create_lstm_network(num_frequency_dimensions, num_hidden_dimensions, num_recurrent_units=1):
     model = Sequential()
     # This layer converts frequency space to hidden space
-    model.add(TimeDistributedDense(input_dim=num_frequency_dimensions, output_dim=num_hidden_dimensions))
+    model.add(TimeDistributed(Dense(input_dim=num_frequency_dimensions, output_dim=num_hidden_dimensions)))
     for cur_unit in xrange(num_recurrent_units):
         model.add(LSTM(input_dim=num_hidden_dimensions, output_dim=num_hidden_dimensions, return_sequences=True))
     # This layer converts hidden space back to frequency space
-    model.add(TimeDistributedDense(input_dim=num_hidden_dimensions, output_dim=num_frequency_dimensions))
+    model.add(TimeDistributed(Dense(input_dim=num_hidden_dimensions, output_dim=num_frequency_dimensions)))
     model.compile(loss='mean_squared_error', optimizer='rmsprop')
     return model
