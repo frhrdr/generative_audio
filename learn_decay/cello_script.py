26951e1eed7c38dc6a49d53295f6bbf617129de7
Fr-d-rik <frederikharder@gmail.com>
2016-06-14 15:21:06 +0200

    script to server...

--- learn_decay/cello_script.py
+++ learn_decay/cello_script.py
@@ -3,11 +3,14 @@ from train_func import train_func
 
 train_dir = 'cello_train'
 _, w_mat_name = train_func(train_dir,
-                        n_hid=1024,
-                        epochs=1,
-                        highest_freq=8000,
-                        n_to_load=2,
-                        down_sampling=True,
-                        save_weights=True)
+                           n_hid=1024,
+                           epochs=150,
+                           highest_freq=8000,
+                           n_to_load=86,
+                           down_sampling=True,
+                           chunks_per_sec=100,
+                           save_weights=True)
+
+print('weights saved to: ', w_mat_name)
 
 
