ce59024ac91d44242d7a43a8658442e28ad8c503
Fr-d-rik <frederikharder@gmail.com>
2016-06-15 11:37:43 +0200

    script to server...

--- learn_decay/cello_script.py
+++ learn_decay/cello_script.py
@@ -9,6 +9,7 @@ _, w_mat_name = train_func(train_dir,
                            n_to_load=86,
                            down_sampling=True,
                            chunks_per_sec=100,
+                           clip_len=7,
                            save_weights=True)
 
 print('weights saved to: ', w_mat_name)
