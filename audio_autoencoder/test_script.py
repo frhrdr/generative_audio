93ad8f36b7417e43dc967940708726a317972420
toologicbv <jorg.sander@toologic.com>
2016-06-16 09:16:11 +0200

    changed initial toy example so that it works with current pipeline processing. we probably need this for the report...when listening to Karen

--- audio_autoencoder/test_script.py
+++ audio_autoencoder/test_script.py
@@ -1,16 +1,21 @@
+from __future__ import print_function
 from audio_preprocessing.pipeline import AudioPipeline
 from ConvAutoencoder import ConvAutoencoder
 
-myAudios = AudioPipeline(1)
 
-batches = myAudios.train_batches()
-# print(next(batches))
+root_to_folder = ""
+train_dir = 'sines_toy_data/'
+data = train_dir + 'sines_mat'
+audios = AudioPipeline(train_dir, n_to_load=86, highest_freq=440,
+                           clip_len=7, mat_dirs=None, chunks_per_sec=1,
+                           down_sampling=True)
+
+batches = audios.train_batches()
 x_data = next(batches).divisible_matrix(16)
-
+print(x_data.shape)
 
 train_audio = x_data[:800, :]
 test_audio = x_data[800:, :]
-
 
 auto = ConvAutoencoder(train_audio, test_audio)
 
