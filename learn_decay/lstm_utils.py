cbf94b1b18e5c44d57db327bf81b712804912391
Fr-d-rik <frederikharder@gmail.com>
2016-06-17 13:19:02 +0200

    server script update

--- learn_decay/lstm_utils.py
+++ learn_decay/lstm_utils.py
@@ -1,8 +1,8 @@
 from keras.models import Sequential
 from keras.layers.wrappers import TimeDistributed
-from keras.layers.core import Dense
+from keras.layers.core import Dense, Flatten
 from keras.layers.recurrent import LSTM
-
+from keras.layers.convolutional import Convolution1D
 
 # taken from MattVitelli's GRUV project
 # https://github.com/MattVitelli/GRUV/blob/master/nn_utils/network_utils.py
@@ -13,6 +13,27 @@ def create_lstm_network(num_frequency_dimensions, num_hidden_dimensions,
     # This layer converts frequency space to hidden space
     model.add(TimeDistributed(Dense(num_hidden_dimensions, activation=l_activation),
                               input_shape=(None, num_frequency_dimensions)))
+    # model.add(TimeDistributedDense(input_dim=num_frequency_dimensions, output_dim=num_hidden_dimensions))
+    for cur_unit in xrange(num_recurrent_units):
+        model.add(LSTM(num_hidden_dimensions, return_sequences=True, stateful=stateful))
+
+    # This layer converts hidden space back to frequency space
+    model.add(TimeDistributed(Dense(input_dim=num_hidden_dimensions, output_dim=num_frequency_dimensions,
+                                    activation=l_activation)))
+    model.compile(loss='mean_squared_error', optimizer='rmsprop')
+    return model
+
+
+def create_conv_lstm_network(num_frequency_dimensions, num_hidden_dimensions,
+                             num_recurrent_units=1, stateful=False, l_activation='linear',
+                             num_filters=1, filter_length=3):
+
+    model = Sequential()
+    # This layer converts frequency space to hidden space
+    model.add(TimeDistributed(Convolution1D(nb_filter=num_filters, filter_length=filter_length,
+                                            activation=l_activation, input_shape=(None, num_frequency_dimensions)),
+                              input_shape=(None, num_frequency_dimensions)))
+    model.add(Flatten())
     # model.add(TimeDistributedDense(input_dim=num_frequency_dimensions, output_dim=num_hidden_dimensions))
     for cur_unit in xrange(num_recurrent_units):
         model.add(LSTM(num_hidden_dimensions, return_sequences=True, stateful=stateful))
--- learn_decay/server_script.py
+++ learn_decay/server_script.py
@@ -1,27 +1,30 @@
 from train_func import train_func
 from generate_seq import gen_seq_full
+train = False
+if train:
+    train_dir = 'guitar_train'
+    _, w_mat_name, d_mat_name = train_func(train_dir,
+                                           n_hid_neurons=2048,
+                                           n_rec_layers=1,
+                                           epochs=100,
+                                           highest_freq=1400,
+                                           n_to_load=45,
+                                           down_sampling=True,
+                                           save_weights=True,
+                                           chunks_per_sec=40,
+                                           clip_len=5,
+                                           add_spectra=True,
+                                           activation='linear')
 
-
-train_dir = 'piano_train'
-_, w_mat_name, d_mat_name = train_func(train_dir,
-                                       n_hid_neurons=2048,
-                                       n_rec_layers=1,
-                                       epochs=100,
-                                       highest_freq=1400,
-                                       n_to_load=45,
-                                       down_sampling=True,
-                                       save_weights=True,
-                                       chunks_per_sec=40,
-                                       clip_len=5,
-                                       add_spectra=True,
-                                       activation='linear')
+    folder_spec = '/instrument_samples/guitar_train/'
+    data = d_mat_name
+    model_name = w_mat_name
+else:
+    model_name = '/weight_matrices/piano_train_45files_5sec_40res_1400maxf_' + \
+                 'spec_m1_2048hid_1lyrs_100ep_linearact'
+    data = 'instrument_samples/piano_train/piano_train_45files_5sec_40res_1400maxf_spec'
 
 folder_spec = '/instrument_samples/guitar_train/'
-data = d_mat_name
-model_name = w_mat_name
-
-# model_name = 'cello_pizz_train_90files_30res_5000maxf_2048hid_100ep'
-# data = 'cello_pizz_train_90files_30res_5000maxf'
 
 prime_length = 40
 num_of_tests = 5
