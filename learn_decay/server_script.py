96ab8b8e0ac078c16c338d595f707e5d53848531
Fr-d-rik <frederikharder@gmail.com>
2016-06-17 12:25:02 +0200

    server script update

--- learn_decay/server_script.py
+++ learn_decay/server_script.py
@@ -4,26 +4,26 @@ from generate_seq import gen_seq_full
 
 train_dir = 'piano_train'
 _, w_mat_name, d_mat_name = train_func(train_dir,
-                                       n_hid_neurons=512,
+                                       n_hid_neurons=2048,
                                        n_rec_layers=1,
-                                       epochs=1,
+                                       epochs=100,
                                        highest_freq=4200,
-                                       n_to_load=8,
+                                       n_to_load=88,
                                        down_sampling=True,
                                        save_weights=True,
-                                       chunks_per_sec=30,
+                                       chunks_per_sec=40,
                                        clip_len=5,
                                        add_spectra=True,
-                                       activation='tanh')
+                                       activation='linear')
 
-folder_spec = '/instrument_samples/cello_pizz_train/'
+folder_spec = '/instrument_samples/piano_train/'
 data = d_mat_name
 model_name = w_mat_name
 
 # model_name = 'cello_pizz_train_90files_30res_5000maxf_2048hid_100ep'
 # data = 'cello_pizz_train_90files_30res_5000maxf'
 
-prime_length = 20
+prime_length = 40
 num_of_tests = 5
 gen_seq_full(folder_spec=folder_spec, data=data, model_name=model_name,
              prime_length=prime_length, num_of_tests=num_of_tests)
