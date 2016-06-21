fe1e839303ded19cfc3d3a5e835e453c8b434688
Fr-d-rik <frederikharder@gmail.com>
2016-06-21 14:11:18 +0200

    serverscript update

--- learn_decay/server_script.py
+++ learn_decay/server_script.py
@@ -3,7 +3,7 @@ from generate_seq import gen_seq_full
 
 train = True
 if train:
-    train_dir = 'guitar_train'
+    train_dir = 'guitar_train/tmp'
     _, w_mat_name, d_mat_name = train_func(train_dir,
                                            n_hid_neurons=512,
                                            n_rec_layers=3,
@@ -19,14 +19,14 @@ if train:
                                            mean_std_per_file=True,
                                            activation='linear')
 
-    folder_spec = '/instrument_samples/guitar_train/tmp/'
+    folder_spec = '/instrument_samples/'
     data = d_mat_name
     model_name = w_mat_name
 else:
     model_name = 'guitar_train_45files_5sec_40res_1400maxf_spec_m1_2048hid_1lyrs_100ep_linearact'
     data = 'guitar_train_45files_5sec_40res_1400maxf_spec'
 
-folder_spec = '/instrument_samples/guitar_train/'
+    folder_spec = '/instrument_samples/'
 
 prime_length = 20
 num_of_tests = 4
