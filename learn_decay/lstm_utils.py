88c6432c7ab918bc03803171fdd3dbd791680c00
toologicbv <jorg.sander@toologic.com>
2016-06-21 15:22:05 +0200

    Third convolutional RNN model...that does not work

--- learn_decay/lstm_utils.py
+++ learn_decay/lstm_utils.py
@@ -1,9 +1,9 @@
 from keras.models import Sequential
 from keras.layers.wrappers import TimeDistributed
-from keras.layers.core import Dense, Flatten
+from keras.layers.core import Dense, Flatten, Reshape
 from keras.layers.recurrent import LSTM
 from keras.layers.convolutional import Convolution1D
-from keras.layers import BatchNormalization, MaxPooling1D
+from keras.layers import BatchNormalization, MaxPooling1D, UpSampling1D
 
 
 # taken from MattVitelli's GRUV project
@@ -30,20 +30,60 @@ def create_conv_lstm_network(num_frequency_dimensions, num_hidden_dimensions,
                                             num_recurrent_units=1, stateful=False):
 
     model = Sequential()
-    model.add(TimeDistributed(Convolution1D(32, 35, border_mode='same', name="1_conv1d", activation='relu'),
+    model.add(TimeDistributed(Convolution1D(32, 41, border_mode='same', name="1_conv1d", activation='relu'),
                               input_shape=(None, num_frequency_dimensions, 1), name="1_timedist"))
     model.add(TimeDistributed(BatchNormalization()))
     model.add(TimeDistributed(MaxPooling1D(2, border_mode='valid')))
-    model.add(TimeDistributed(Convolution1D(16, 7, activation='relu', border_mode='same')))
+    model.add(TimeDistributed(Convolution1D(16, 17, activation='relu', border_mode='same')))
     model.add(TimeDistributed(BatchNormalization()))
     model.add(TimeDistributed(MaxPooling1D(2, border_mode='valid')))
-    model.add(TimeDistributed(Convolution1D(4, 3, activation='relu', border_mode='same')))
+    model.add(TimeDistributed(Convolution1D(8, 3, activation='relu', border_mode='same')))
     model.add(TimeDistributed(BatchNormalization()))
     model.add(TimeDistributed(MaxPooling1D(2, border_mode='valid')))
     model.add(TimeDistributed(Flatten()))
-
     for cur_unit in xrange(num_recurrent_units):
         model.add(LSTM(output_dim=num_hidden_dimensions, return_sequences=True, stateful=stateful))
     model.add(TimeDistributed(Dense(input_dim=num_hidden_dimensions, output_dim=num_frequency_dimensions)))
     model.compile(loss='mean_squared_error', optimizer='rmsprop')
+    return model
+
+
+def create_autoconv_lstm_network(num_frequency_dimensions, num_hidden_dimensions,
+                                 num_recurrent_units=1, stateful=False):
+
+    model = Sequential()
+    model.add(TimeDistributed(Convolution1D(32, 41, border_mode='same', name="1_conv1d", activation='relu'),
+                              input_shape=(None, num_frequency_dimensions, 1), name="1_timedist"))
+    model.add(TimeDistributed(BatchNormalization()))
+    model.add(TimeDistributed(MaxPooling1D(2, border_mode='valid')))
+    model.add(TimeDistributed(Convolution1D(16, 17, activation='relu', border_mode='same')))
+    model.add(TimeDistributed(BatchNormalization()))
+    model.add(TimeDistributed(MaxPooling1D(2, border_mode='valid')))
+    model.add(TimeDistributed(Convolution1D(8, 3, activation='relu', border_mode='same')))
+    model.add(TimeDistributed(BatchNormalization()))
+    model.add(TimeDistributed(MaxPooling1D(2, border_mode='valid')))
+    last_2d_shape = model.output_shape[-2:]
+    output_num_rnn = last_2d_shape[0] * last_2d_shape[1]
+
+    model.add(TimeDistributed(Flatten()))
+
+    for cur_unit in xrange(num_recurrent_units):
+        model.add(LSTM(output_dim=num_hidden_dimensions, return_sequences=True, stateful=stateful))
+    model.add(TimeDistributed(Dense(input_dim=num_hidden_dimensions, output_dim=output_num_rnn)))
+
+    model.add(TimeDistributed(Reshape(last_2d_shape)))
+    model.add(TimeDistributed(Convolution1D(8, 3, activation='relu', border_mode='same')))
+    model.add(TimeDistributed(BatchNormalization()))
+    model.add(TimeDistributed(UpSampling1D(2)))
+    model.add(TimeDistributed(Convolution1D(16, 17, activation='relu', border_mode='same')))
+    model.add(TimeDistributed(BatchNormalization()))
+    model.add(TimeDistributed(UpSampling1D(2)))
+    model.add(TimeDistributed(Convolution1D(32, 41, activation='relu', border_mode='same')))
+    model.add(TimeDistributed(BatchNormalization()))
+    model.add(TimeDistributed(UpSampling1D(2)))
+    model.add(TimeDistributed(Convolution1D(1, 3, activation='tanh', border_mode='same')))
+    last_2d_shape = model.output_shape[-2]
+    model.add(TimeDistributed(Reshape((last_2d_shape,))))
+    model.compile(loss='mean_squared_error', optimizer='rmsprop')
+
     return model
