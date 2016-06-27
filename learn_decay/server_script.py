da9b3c8237943720c02bad41147b5ee82aaaacaa
toologicbv <jorg.sander@toologic.com>
2016-06-27 14:14:38 +0200

    Update server_script.py

--- learn_decay/server_script.py
+++ learn_decay/server_script.py
@@ -14,7 +14,7 @@ from generate_seq import gen_seq_full
     this script can be used to train and generate one of the following RNN models
     (1) architecture='1': simple LSTM model
         
-    (2) architecture='2': CNN in front of an LSTM model
+    (2) architecture='2': CNN combined with an LSTM model
     
     Some explanations in regards to the parameters/variables:
         (1) train_dir: points to a directory that contains the wav sound files the model is being trained on.
@@ -32,7 +32,11 @@ from generate_seq import gen_seq_full
                              directory:
                              guitar_train_45files_5sec_60res_1400maxf_spec_m1_512hid_1lyrs_180ep_linearact_model.json
                              guitar_train_45files_5sec_60res_1400maxf_spec_m1_512hid_1lyrs_180ep_linearact_weights.h5
-                             
+        (4) prime_length: number of time slices used for the prime signal. E.g. the above mentioned model expects
+                          sound signals that are separated into 60 time slices/second. 
+                          We found that if we use 1/3 of a second (in this case prime_length=20) as prime this results 
+                          most oftnen in reasonable results while generating new sequences.
+        (5) num_of_tests: the number of test
 """
 
 train = True
@@ -41,17 +45,17 @@ if train:
     _, w_mat_name, d_mat_name = train_func(train_dir,
                                            n_hid_neurons=512,
                                            n_rec_layers=1,
-                                           epochs=180,
-                                           highest_freq=1400,
-                                           n_to_load=4,
+                                           epochs=180,              
+                                           highest_freq=1400,       # highest frequency of the specific instrument
+                                           n_to_load=45,            # number of wav files to load for training
                                            down_sampling=True,
                                            save_weights=True,
-                                           chunks_per_sec=90,
-                                           clip_len=5,
+                                           chunks_per_sec=90,       # number of time slices per second
+                                           clip_len=5,              # clip/padd the sound signal to this length in seconds
                                            add_spectra=False,
                                            architecture='1',
-                                           mean_std_per_file=True,
-                                           activation='linear')
+                                           mean_std_per_file=True,  # True=calc global mean/stddev; False=calc mean/stddev for each file
+                                           activation='linear')     # activation function used in dense layers
 
     folder_spec = '/instrument_samples/guitar_train/'
     data = d_mat_name
@@ -63,6 +67,6 @@ else:
     folder_spec = '/instrument_samples/guitar_train/'
 
 prime_length = 40
-num_of_tests = 4
+num_of_tests = 2
 gen_seq_full(folder_spec=folder_spec, data=data, model_name=model_name,
              prime_length=prime_length, num_of_tests=num_of_tests, add_spectra=False, mean_std_per_file=True)
